743b623b847e245b22c71d7591d62bf628b25990
qbadmin <qbadmin@qbadmin-desktop.(none)>
2015-03-30 15:35:12 +0530

    Dictionary: shows random words with their meaning.

--- notify.rb
+++ notify.rb
@@ -1,11 +1,17 @@
 require 'libnotify'
+require 'json'
 
+file = File.read('/home/qbadmin/Desktop/hobby/time-notifier/dictionary-master/dictionary.json')
+data_hash = JSON.parse(file)
 
- while true    
+ 
+
+ while true 
+ 	key = data_hash.keys[rand(0..86035)]   
 	n = Libnotify.new do |notify|
-	  notify.summary    = "Time"
-	  notify.body       = Time.now.strftime("%d/%m/%Y %I:%M%p")  
-	  notify.timeout    = 1.5         # 1.5 (s), 1000 (ms), "2", nil, false
+	  notify.summary    = key
+	  notify.body       = data_hash[key]
+	  notify.timeout    = 1.5        # 1.5 (s), 1000 (ms), "2", nil, false
 	  notify.urgency    = :critical   # :low, :normal, :critical
 	  notify.append     = false       # default true - append onto existing notification
 	  notify.transient  = true        # default false - keep the notifications around after display
